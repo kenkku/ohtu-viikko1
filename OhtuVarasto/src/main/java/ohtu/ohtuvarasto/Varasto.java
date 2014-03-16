b347c50d5460405b15d0b23392fded014fac2c0a
Tero Keinänen <kenkku@kenkku.net>
2014-03-16 23:35:55 +0200

    Revert "särki"
    This reverts commit f2b454bfd82d34c3fb262a73d5bc515745c14bdc.

--- OhtuVarasto/src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ OhtuVarasto/src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -49,7 +49,7 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara > 0) // virhetilanteessa voidaan tehdä
+        if (maara < 0) // virhetilanteessa voidaan tehdä 
         {
             return;       // tällainen pikapoistuminenkin!
         }
